e1d6964d4594616d36cfdb3bad04f74741a6a2d5
DreSean-Sutton <dreseansutton@gmail.com>
2022-09-05 23:43:01 -0700

    Database is now correctly connecting

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,9 @@
-
 const express = require('express');
 require('dotenv').config({ path: './.env' });
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 5000;
 const dbo = require('./db/conn');
-
 app.listen(port, () => {
   dbo.connectToServer(function (err: any) {
     if(err) console.error(err);
