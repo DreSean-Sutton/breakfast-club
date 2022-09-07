b3442515e40f9843821f5be9d088d04b452e9279
DreSean-Sutton <dreseansutton@gmail.com>
2022-09-06 23:21:10 -0700

    Moved all files into breakfast-club folder

--- breakfast-club/server/server.ts
+++ breakfast-club/server/server.ts
@@ -1,6 +1,5 @@
-import axios from 'axios';
 const express = require('express');
-require('dotenv').config({ path: './.env' });
+require('dotenv').config({ path: '../.env' });
 const dbo = require('./db/conn');
 const app = express();
 const port = process.env.PORT || 5000;
