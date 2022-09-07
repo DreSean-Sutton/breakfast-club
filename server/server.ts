179b68ff379d4184b4500dacad5dd851cd1d428a
DreSean-Sutton <dreseansutton@gmail.com>
2022-09-06 22:19:46 -0700

    Saving before I change the structure of my entire document

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,11 @@
 const express = require('express');
 require('dotenv').config({ path: './.env' });
-const app = express();
-app.use(express.json());
-const port = process.env.PORT || 5000;
 const dbo = require('./db/conn');
+const app = express();
+const port = process.env.PORT || 5000;
+import axios from 'axios';
+app.use(express.json())
+
 app.listen(port, () => {
   dbo.connectToServer(function (err: any) {
     if(err) console.error(err);
